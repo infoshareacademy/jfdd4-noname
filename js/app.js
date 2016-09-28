e73e1c38c94c3379460f4416c0712593eeb07ca2
Piotr Szefler <piotr.szefler@gmail.com>
2016-09-28 16:01:29 +0200

    responsive dropdown menu (complete)

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@
 $('.menu-trigger a').click(function() {
     $('#menu').toggleClass('active');
 });
+
+$(window).resize(function () {
+   if ($(window).width() >= 900) {
+       $('#menu').removeClass('active');
+   }
+});
+
+$('#menu').find('li>a').click(function () {
+    $('#menu').removeClass('active');
+});
