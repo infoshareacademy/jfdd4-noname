cda04d4d7909aa610894d95ba15e43b736885baf
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-01 01:53:42 +0200

    change colours, add intro game, cleaning

--- js/gamepam.js
+++ js/gamepam.js
@@ -1,41 +1,50 @@
-var size = 10;
-
-
-$(document).ready(function () {
-
-
-    var $form = $('input.button');
-    $form.on('click', function (event) {
-        event.preventDefault();
-        $('.game-area').removeClass('game-area').addClass('game-start');
-
-
-        var $row, $cell;
-
-        for (var rowNum = 0; rowNum < size; rowNum++) {
-            $row = $('<tr>').appendTo('#game');
-            for (var cellNum = 0; cellNum < size; cellNum++) {
-                $cell = $('<td>').addClass('cell').appendTo($row);
-
-                // var isBlack = rowNum % 2 ? cellNum % 2 : !(cellNum % 2);
-
-                // if (isBlack) {
-                //     $cell.addClass('black')
-                //         .attr('data-color', 'jestem czarny!!');
-                //
-                // } else {
-                //     $cell.addClass('white').attr('data-color', 'jestem bialy!!');
-                // }
-
-
-            }
-        }
-
-    $('#game').find('tr').eq().addClass('road');
-    $('#game').find('tr').eq(3).addClass('road');
-    $('#game').find('tr').eq(4).addClass('road');
-
-
-    });
-});
-
+// var size = 10;
+//
+//
+// $(document).ready(function () {
+//
+//     // Initialize
+//
+//
+//     var $form = $('input.button');
+//     $form.on('click', function (event) {
+//         event.preventDefault();
+//         $('.game-area').removeClass('game-area').addClass('game-start');
+//
+//
+//         var $row, $cell, inc=1, incTwo=0;
+//
+//         for (var rowNum = 0; rowNum < size; rowNum++) {
+//             $row = $("<tr id=" + incTwo  + "></tr>").appendTo('#game');
+//             incTwo++;
+//             for (var cellNum = 0; cellNum < size; cellNum++) {
+//                 $cell = $("<td id=" + incTwo+ inc + "></td>").addClass('cell').appendTo($row);
+//                 inc++;
+//                 // var isBlack = rowNum % 2 ? cellNum % 2 : !(cellNum % 2);
+//
+//                 // if (isBlack) {
+//                 //     $cell.addClass('black')
+//                 //         .attr('data-color', 'jestem czarny!!');
+//                 //
+//                 // } else {
+//                 //     $cell.addClass('white').attr('data-color', 'jestem bialy!!');
+//                 // }
+//
+//
+//             }
+//         }
+//
+//
+//         ///Adding img bus
+//
+// function moveBus() {
+//
+// }
+//
+//
+//     });
+//
+//
+//
+// });
+//
