4f2297b14e2a07f9f51754ec46f978cce210dff0
piotrszefler <piotr.szefler@gmail.com>
2016-10-02 11:19:58 +0200

    add districts dynamically

--- js/game.js
+++ js/game.js
@@ -8,11 +8,13 @@ $("#join").submit(function(e) {
 
 // CREATE GAME BOARD
 
-// $('input.button').click(function () {
-//     $('#game').removeClass('inactive');
-//     window.location = '#game';
+$('input.button').click(function () {
+    $('#game').removeClass('inactive');
+    $('#join').addClass('inactive');
+    // window.location = '#game';
 
-    var boardSize = 20,
+    var boardSize = 32,
+        fieldsize = 100 / boardSize + '%',
         $row;
 
 
@@ -20,61 +22,81 @@ $("#join").submit(function(e) {
         $row = $('<tr class="board-row">').appendTo('#board');
 
         for (j = 0; j < boardSize; j++) {
-            $('<td class="board-cell">').appendTo($row)
+            $('<td class="board-cell">').css('width', fieldsize).appendTo($row)
         }
     }
 
 
-// });
+    // PLACE AND MOVE PASSENGER
+    var $boardRow = $('.board-row'),
+        $boardCell = $('.board-cell');
+    $passenger = $('#passenger');
 
-// PUT PASSENGER ON THE BOARD
-var $passRow = $('.board-row'),
-    $passCell = $('.board-cell');
-    $passenger = $('td#passenger');
 
-//current position
-var passRowIndex = 0,
-    passCellIndex = 0;
+    //current position
+    var passRowIndex = 0,
+        passCellIndex = 0;
 
-$passRow.eq(passRowIndex).find($passCell).eq(passCellIndex).attr('id', 'passenger');
-
-// MOVE PASSENGER WITH ARROWS
-
-$(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: // left
-            if (passCellIndex > 0) {
-                $passCell.removeAttr('id');
-                passCellIndex -= 1;
-                $passRow.eq(passRowIndex).find($passCell).eq(passCellIndex).attr('id', 'passenger');
-            }
-            break;
-
-        case 38: // up
-            if (passRowIndex > 0) {
-                $passCell.removeAttr('id');
-                passRowIndex -= 1;
-                $passRow.eq(passRowIndex).find($passCell).eq(passCellIndex).attr('id', 'passenger');
-            }
-            break;
-
-        case 39: // right
-            if (passCellIndex < 19) {
-                $passCell.removeAttr('id');
-                passCellIndex += 1;
-                $passRow.eq(passRowIndex).find($passCell).eq(passCellIndex).attr('id', 'passenger');
-            }
-            break;
-
-        case 40: // down
-            if (passRowIndex < 19) {
-                $passCell.removeAttr('id');
-                passRowIndex += 1;
-                $passRow.eq(passRowIndex).find($passCell).eq(passCellIndex).attr('id', 'passenger');
-            }
-            break;
-
-        default: return; // exit this handler for other keys
+    function movePassenger() {
+        $('#passenger').removeAttr('id');
+        $boardRow.eq(passRowIndex).find($boardCell).eq(passCellIndex).attr('id', 'passenger');
     }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
+
+    movePassenger();
+
+    // MOVE PASSENGER WITH ARROWS
+    $(document).keydown(function(e) {
+
+
+        switch(e.which) {
+            case 37: // left
+                if (passCellIndex > 0) {
+                    passCellIndex -= 1;
+                    movePassenger();
+                }
+                break;
+
+            case 38: // up
+                if (passRowIndex > 0) {
+                    passRowIndex -= 1;
+                    movePassenger();
+                }
+                break;
+
+            case 39: // right
+                if (passCellIndex < boardSize - 1) {
+                    passCellIndex += 1;
+                    movePassenger();
+                }
+                break;
+
+            case 40: // down
+                if (passRowIndex < boardSize - 1) {
+                    passRowIndex += 1;
+                    movePassenger();
+                }
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
+    // CREATE DISTRICTS
+    var districtStart = 0,
+        distrctEnd = boardSize -1;
+
+    for (var i = districtStart; i <= distrctEnd; i++) {
+        if (i > 1 && i < 10 || i > 11 && i < 20 || i > 21 && i < 30) {
+            for (var j = districtStart; j <= distrctEnd; j++) {
+                if (j > 1 && j < 10 || j > 11 && j < 20 || j > 21 && j < 30) {
+                    $boardRow.eq(i).find($boardCell).eq(j).addClass('district');
+                }
+            }
+        }
+
+    }
+
+
 });
+
