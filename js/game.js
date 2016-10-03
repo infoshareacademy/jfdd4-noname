944f430cec0b9bc82cbcfd08f53700ad23184101
Piotr Szefler <piotr.szefler@gmail.com>
2016-10-03 16:17:08 +0200

    modyfy game board (2x2 districts)

--- js/game.js
+++ js/game.js
@@ -13,7 +13,7 @@ $('#game').removeClass('inactive');
 $('#join').addClass('inactive');
 // window.location = '#game';
 
-var boardSize = 32, //must be (number divisible by 3) + 8
+var boardSize = 32, //must be even
     fieldsize = 100 / boardSize + '%',
     $row;
 
@@ -82,15 +82,14 @@ $(document).keydown(function (e) {
 });
 
 // CREATE DISTRICTS
-var districtStart = 2,
-    districtSize = (boardSize - 8) / 3,
-    boardEnd = boardSize - 1;
+var roadSize = 2,
+    districtSize = (boardSize - 2) / 2;
 
-
-for (var i = districtStart; i <= boardEnd; i++) {
-    if (i >= districtStart && i < districtStart + districtSize || i >= 2 * districtStart + districtSize && i < 2 * districtStart + 2 * districtSize || i >= 3 * districtStart + 2 * districtSize && i < 3 * districtStart + 3 * districtSize) {
-        for (var j = districtStart; j <= boardEnd; j++) {
-            if (j >= districtStart && j < districtStart + districtSize || j >= 2 * districtStart + districtSize && j < 2 * districtStart + 2 * districtSize || j >= 3 * districtStart + 2 * districtSize && j < 3 * districtStart + 3 * districtSize) {
+    
+for (var i = 0; i < boardSize; i++) {
+    if (i < districtSize || i >= districtSize + roadSize) {
+        for (var j = 0; j < boardSize; j++) {
+            if (j < districtSize || j >= districtSize + roadSize) {
                 $boardRow.eq(i).find($boardCell).eq(j).addClass('district');
             }
         }
@@ -98,39 +97,39 @@ for (var i = districtStart; i <= boardEnd; i++) {
 }
 
 // CREATE AND MOVE BUS
-var busSpeed = 500;
-
-function visualizeBusPosition(busRowIndex, busCellIndex) {
-    $('.bus').removeClass('bus');
-    $boardRow.eq(busRowIndex).find($boardCell).eq(busCellIndex).addClass('bus');
-}
-
-
-
-
-function callbackVisualizeBusPosition(busRowIndex, busCellIndex){
-    return function(){
-        console.log(busRowIndex, busCellIndex);
-        visualizeBusPosition(busRowIndex, busCellIndex);
-    }
-}
-
-
-function moveBus() {
-    console.time('loop'); //start time count
-    var busRowIndex = 0, busCellIndex = 0;
-    visualizeBusPosition(busRowIndex, busCellIndex);
-    for (var x = 1; x < boardSize; x++) {
-        busRowIndex += 1;
-        setTimeout(callbackVisualizeBusPosition(busRowIndex, busCellIndex), busSpeed * x);
-    }
-    console.timeEnd('loop'); // end time count
-}
-moveBus();
-setInterval(function () {
-    moveBus();
-
-}, 16000);
+// var busSpeed = 500;
+//
+// function visualizeBusPosition(busRowIndex, busCellIndex) {
+//     $('.bus').removeClass('bus');
+//     $boardRow.eq(busRowIndex).find($boardCell).eq(busCellIndex).addClass('bus');
+// }
+//
+//
+//
+//
+// function callbackVisualizeBusPosition(busRowIndex, busCellIndex){
+//     return function(){
+//         console.log(busRowIndex, busCellIndex);
+//         visualizeBusPosition(busRowIndex, busCellIndex);
+//     }
+// }
+//
+//
+// function moveBus() {
+//     console.time('loop'); //start time count
+//     var busRowIndex = 0, busCellIndex = 0;
+//     visualizeBusPosition(busRowIndex, busCellIndex);
+//     for (var x = 1; x < boardSize; x++) {
+//         busRowIndex += 1;
+//         setTimeout(callbackVisualizeBusPosition(busRowIndex, busCellIndex), busSpeed * x);
+//     }
+//     console.timeEnd('loop'); // end time count
+// }
+// moveBus();
+// setInterval(function () {
+//     moveBus();
+//
+// }, 16000);
 
 
 // });
