d76a3cec39f8844034ce70ee6bdab18092187df0
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-06 21:19:27 +0200

    update - changing menu on class active during scroll

--- js/menu-app.js
+++ js/menu-app.js
@@ -2,6 +2,8 @@
  * Created by Pamela on 2016-10-06.
  */
 
+//SCROLL
+
 $(document).ready(function () {
 
     $(window).scroll(function () {
@@ -20,18 +22,98 @@ $(document).ready(function () {
         $(this).addClass('active-scroll');
     });
 
-    $('a[href^="#"]').on('click', function (e) { // find all elements with a title attribute value starting with "#"
-        e.preventDefault();
-        // $(document).off('scroll');
+//HIGHLIGHT THE ACTIVE MENU
 
-        $('a').each(function () {
-            $(this).removeClass('active');})
+    var lastId,
+        menu = $("#menu"),
+        menuHeight = menu.outerHeight() + 15,
+        // All list items
+        menuElem = menu.find("a"),
+        // Anchors corresponding to menu items
+        scrollElem = menuElem.map(function () {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            } // it returns 5 sekcji "#"
+
         });
-        $(this).addClass('active');
-        var section = this.hash;
 
 
+//scroll animation
+    menuElem.click(function (e) {
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top - menuHeight + 1;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 400);
+        e.preventDefault();
+    });
+
+//bind to scroll
+    $(window).scroll(function () {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + menuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollElem.map(function () {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            menuElem
+                .parent().removeClass("active-link")
+                .end().filter("[href='#" + id + "']")
+                .parent().addClass("active-link");
+        }
+    });
+
+
+    // $(document).on("scroll", activeMenu);
+    // $('a[href^="#"]').on('click', function (e) {
+    //     e.preventDefault();
+    //     $(document).off("scroll");
+    //
+    //     $('a').each(function () {
+    //         $(this).removeClass('active');
+    //
+    //     });
+    //     $(this).addClass('active');
+    //     var target = this.hash,
+    //         menu = target;
+    //     $target = $(target);
+    //     $('html, body').stop().animate({
+    //         'scrollTop': $target.offset().top
+    //     }, 500, 'swing', function () {
+    //         window.location.hash = target;
+    //         $(document).on("scroll", activeMenu);
+    //     });
+    // });
+    //
+    // function activeMenu(event) {
+    //     $('#menu li a').each(function () {
+    //         var scrollPosTop = $(document).scrollTop();
+    //         var currentSection = $(this);
+    //         var refSection = $(currentSection.attr("href")).position().top;
+    //         if (refSection <= scrollPosTop && refSection > scrollPosTop) {
+    //             $('#menu li a').removeClass('active');
+    //             currentSection.addClass('active');
+    //         } else {
+    //             currentSection.removeClass('active');
+    //         }
+    //     })
+    // }
 
 });
+
+
+
+
+
+
 
 
