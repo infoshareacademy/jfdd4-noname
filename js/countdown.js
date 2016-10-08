869a04ddee39dd7be8f63ba15e84a5385555c9ab
YOUR NAME <godlewskikar@gmail.com>
2016-10-08 19:20:33 +0200

    timer v0.2 - timer placed at correct position - top of #gameBoard - removed timer "speed up" glitch

--- js/countdown.js
+++ js/countdown.js
@@ -2,31 +2,28 @@
  * Created by kgodlewski on 06.10.16.
  */
 var timeLeft;
+var timerClock;
 
 insertTimer();
 
 function insertTimer() {
     $('<div class="timerClock">').appendTo('#gameBoard');
     $('<button type="button" name="start" id="startClock">').text('START GAME').appendTo('.timerClock');
-    $('<div>').addClass('timeLeft').text(timeLeft).appendTo('.timerClock');
+    $('<div>').attr('id','timeLeft').text(timeLeft).prependTo('#gameBoard');
 }
 function countDown() {
     timeLeft -= 0.1;
     if (timeLeft<0){
         return 0;
     }
-    $('.timeLeft').text(timeLeft.toFixed(1));
-}
-
-function setClock() {
-    clearInterval(countDown);
-    setInterval(countDown,100);
+    $('#timeLeft').text(timeLeft.toFixed(1));
 }
 
 $('#startClock').on("click", function () {
-    timeLeft = 10;
+    clearInterval(timerClock);
+    timeLeft = 360;
+    timerClock = setInterval(countDown,100);
     countDown();
-    setClock();
 });
 
 
